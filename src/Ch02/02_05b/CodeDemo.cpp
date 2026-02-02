07db29d35d9d89c3c3bd96a5069bcd224f742181
Charmaine Washington <cwashington2011@outlook.com>
2026-02-01 20:39:00 -0800

    fixed error

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -14,7 +14,13 @@ int main(){
     auto f = 'd';
     auto g = "C++ rocks!";
 
-    
+    std::cout << "The type of a is " << typeid(a).name() << std::endl;
+    std::cout << "The type of b is " << typeid(b).name() << std::endl;
+    std::cout << "The type of c is " << typeid(c).name() << std::endl;
+    std::cout << "The type of d is " << typeid(d).name() << std::endl;
+    std::cout << "The type of e is " << typeid(e).name() << std::endl;
+    std::cout << "The type of f is " << typeid(f).name() << std::endl;
+    std::cout << "The type of g is " << typeid(g).name() << std::endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
