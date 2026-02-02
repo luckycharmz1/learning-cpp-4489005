a10a61844d1571dd7130c4b06a3b7b62db403cb4
Charmaine Washington <cwashington2011@outlook.com>
2026-02-01 19:39:12 -0800

    chp.2

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -13,6 +13,13 @@ int main(){
     std::cout << "a = " << a << std::endl;
     std::cout << "b = " << b << std::endl;
     std::cout << "flag = " << my_flag << std::endl;
+    my_flag = true;
+    std::cout << "flag = " << my_flag << std::endl;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "b - a = " << b - a << std::endl;
+    unsigned int positive; 
+    positive = b - a;
+    std::cout << "b - a (unsigned) = " << positive << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
