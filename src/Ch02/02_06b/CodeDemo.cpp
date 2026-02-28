b62beac01b0d7d5fc3d2d19a2ee05067de4ae7bf
Charmaine Washington <cwashington2011@outlook.com>
2026-02-27 20:29:44 -0800

    Update CodeDemo.cpp

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -4,9 +4,18 @@
 
 #include <iostream>
 #include <string>
+#include <cstdin>
+
+#define CAPACITY 5000
 
 int main(){
-    
+    int32_t large = CAPACITY
+    uint8_t small = 37;
+#ifdef DEBUG
+    std::cout << "[About to perform the addition]" << std::endl;
+#endif 
+    large += small; //large = large + small;
+    std::cout << "The large integer is" << large << std::endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
