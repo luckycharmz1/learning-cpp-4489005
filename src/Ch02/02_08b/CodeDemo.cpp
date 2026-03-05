4660aa487f7c2de41e899bc0dab8287f609ce76c
Charmaine Washington <cwashington2011@outlook.com>
2026-03-04 22:11:52 -0800

    Update CodeDemo.cpp

--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -5,6 +5,12 @@
 #include <iostream>
 
 int main(){
+    int age[4];
+
+    age[0] = 25;
+    age[1] = 20;
+    age[2] = 19;
+    age[3] = 19;
     
     std::cout << std::endl << std::endl;
     return (0);
